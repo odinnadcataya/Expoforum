13e90170aef38cb5910cede6fbd4a0ae751c4042
odinnadcataya <karina_v91@mail.ru>
2022-01-30 15:44:54 +1000

    new

---
+++ js/style.js
@@ -0,0 +1,27 @@
+const navBtn = document.querySelector('.header__nav-btn');
+const mobileNav = document.querySelector('.header__mobile-nav');
+const body = document.body;
+
+//клик по кнопке
+navBtn.addEventListener('click', function (event) {
+    event.stopPropagation();
+    toggleMobileNav();
+})
+
+//клик по окну за пределами навигации
+window.addEventListener('click', function () {
+    if (body.classList.contains('no-scroll')) {
+        toggleMobileNav();
+    }
+})
+
+//Останавливаем клик внутри открытой мобильной навигации
+mobileNav.addEventListener('click', function (event){
+    event.stopPropagation ();
+})
+
+function toggleMobileNav() {
+    mobileNav.classList.toggle('header__mobile-nav_hidden');
+    navBtn.classList.toggle('header__nav-btn_close');
+    body.classList.toggle('no-scroll');
+}
